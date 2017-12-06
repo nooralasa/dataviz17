20de8503720e26f3edc0b64d2b6116997f5f4450
Noor Eddin Amer <nooralasa@gmail.com>
2017-12-06 08:51:53 -0500

    First attempt multicell

--- d3Plot.js
+++ d3Plot.js
@@ -10,10 +10,11 @@ var margin = {top: 20, right: 20, bottom: 30, left: 40},
  */ 
 
 var time = 76
+var colorGroup = 0
 
 // setup x 
 var xValue = function(d) { return d["Calcium"];}, // data -> value
-    xScale = d3.scale.linear().range([0, width]), // value -> display
+    xScale = d3.scale.linear().range([0, width-50]), // value -> display
     xMap = function(d) { return xScale(xValue(d));}, // data -> display
     xAxis = d3.svg.axis().scale(xScale).orient("bottom");
 
@@ -24,8 +25,12 @@ var yValue = function(d) { return d["Voltage"];}, // data -> value
     yAxis = d3.svg.axis().scale(yScale).orient("left");
 
 // setup fill color
-var cValue = function(d) { return d.Protein;},
-    color = d3.scale.category20c();
+var cValue = function(d) { 
+  colorGroup += 1
+  return d.Cell+"-"+d.Protein;
+}
+
+var color = d3.scale.category20c();
 
 // add the graph canvas to the body of the webpage
 var svg = d3.select("#plot").append("svg")
@@ -39,21 +44,23 @@ var tooltip = d3.select("body").append("div")
     .attr("class", "tooltip")
     .style("opacity", 0);
 
-function shapeFromData(d) {
-  console.log(d)
-  return d3.svg.symbol().type("circle")
-}
+// function shapeFromData(d) {
+//   console.log(d)
+//   return d3.svg.symbol().type("circle")
+// }
 
 // load data
-d3.csv("data/sample_data.csv", function(error, data) {
+d3.csv("data/allCells.csv", function(error, data) {
 
   // change string (from CSV) into number format
   data.forEach(function(d, i) {
     d["Calcium"] = +d["Calcium"];
     d["Voltage"] = +d["Voltage"];
-    d["Time (min)"] = +d["Time (min)"];
-//    console.log(d);
+    d["Time"] = +d["Time"];
+    // console.log(i, d);
   });
+
+  console.log("data", data)
 
   // don't want dots overlapping axis, so add in buffer to data domain
   xScale.domain([d3.min(data, xValue)-.1, d3.max(data, xValue)+.1]);
@@ -66,10 +73,10 @@ d3.csv("data/sample_data.csv", function(error, data) {
       .call(xAxis)
     .append("text")
       .attr("class", "label")
-      .attr("x", width)
+      .attr("x", width-50)
       .attr("y", -6)
       .style("text-anchor", "end")
-      .text("Voltage");
+      .text("Calcium");
 
   // y-axis
   svg.append("g")
@@ -81,25 +88,26 @@ d3.csv("data/sample_data.csv", function(error, data) {
       .attr("y", 6)
       .attr("dy", ".71em")
       .style("text-anchor", "end")
-      .text("Calcium");
+      .text("Voltage");
 
 
   // draw dots
   svg.selectAll(".dot")
       .data(data)
     .enter().append("path")
-      .attr("id", function(d) { return "dot-"+d["Time (min)"]})
+      .attr("id", function(d) { return "dot-"+d["Time"]+d['Cell']})
+      .attr("class", function(d, i) { return "dot-"+d['Cell']})
       .attr("d", d3.svg.symbol()
         .type(function(d) { if (d.Gene=="N") { return "square" } else { return "circle" } })
         .size(function(d) { return d.LFP*10 }))
       .attr("transform", function(d) { return "translate(" + xMap(d) + "," + yMap(d) + ")"; })
       .style("fill", function(d) { return color(cValue(d));}) 
       .on("mouseover", function(d) {
-        if (d["Time (min)"] < time) {
+        if (d["Time"] < time) {
           tooltip.transition()
                .duration(200)
                .style("opacity", .9);
-          tooltip.html("Time: " + d["Time (min)"] +  "<br/> Voltage: " + d["Voltage"] +  "<br/> Calcium: " + d["Calcium"] + "<br/> LFP: " + d["LFP"] + "<br/> Protein: " + d["Protein"] + "<br/> Gene: " + d["Gene"])
+          tooltip.html("Time: " + d["Time"] +  "<br/> Voltage: " + d["Voltage"] +  "<br/> Calcium: " + d["Calcium"] + "<br/> LFP: " + d["LFP"] + "<br/> Protein: " + d["Protein"] + "<br/> Gene: " + d["Gene"])
                .style("left", (d3.event.pageX + 5) + "px")
                .style("top", (d3.event.pageY - 28) + "px");
         }
@@ -119,36 +127,171 @@ d3.csv("data/sample_data.csv", function(error, data) {
 
   // draw legend colored rectangles
   legend.append("rect")
-      .attr("x", width - 18)
+      .attr("x", width - 30)
       .attr("width", 18)
       .attr("height", 18)
+      .attr("class", function(d, i) { 
+        if ((i+1)%4==0) {
+          return "hidden"
+        }
+        return null
+      })
       .style("fill", color);
 
   // draw legend text
   legend.append("text")
-      .attr("x", width - 24)
+      .attr("x", width - 36)
       .attr("y", 9)
       .attr("dy", ".35em")
       .style("text-anchor", "end")
-      .text(function(d) { return d;})
+      .attr("class", function(d, i) { 
+        if ((i+1)%4==0) {
+          return "hidden"
+        }
+        return null
+      })
+      .text(function(d, i) { 
+        if ((i+1)%4==1) { return "H" } 
+        if ((i+1)%4==2) { return "M" } 
+        if ((i+1)%4==3) { return "L" } 
+      });
+
+  // draw legend cell #
+  legend.append("text")
+      .attr("x", width + 20)
+      .attr("y", 9)
+      .attr("dy", ".35em")
+      .style("text-anchor", "end")
+      .attr("class", function(d, i) { 
+        if ((i+1)%4==0) {
+          return "hidden"
+        }
+        return null
+      })
+      .text(function(d, i) { 
+        if ((i+1)%4==2) { return "Cell "+(parseInt(i/4)+1) } 
+      });
 });
 
 function updatePlot() {
 
   // load data
-  d3.csv("data/sample_data.csv", function(error, data) {
+  d3.csv("data/allCells.csv", function(error, data) {
 
     var svg = d3.select("#plot").transition();
 
     data.forEach(function(d, i) {
-      if (d["Time (min)"] < time) {
-        svg.select('[id="dot-'+d['Time (min)']+'"]').attr("class", null);
+      if (d["Time"] < time) {
+        svg.select('[id="dot-'+d['Time']+d['Cell']+'"]').attr("class", null);
       } else {
-        svg.select('[id="dot-'+d['Time (min)']+'"]').attr("class", "hidden");
+        svg.select('[id="dot-'+d['Time']+d['Cell']+'"]').attr("class", "hidden");
       }
+
+      // if (d["Cell"] != "Cell 2") {
+      //   svg.select('[id="dot-'+d['Time']+d['Cell']+'"]').attr("class", "hidden");
+      // }
     });
 
   });
+}
+
+/* Simple, reusable slider in pure d3 */
+
+function simpleSlider () {
+
+    var width = 100,
+        value = 0.5, /* Domain assumes to be [0 - 1] */
+        event,
+        x = 0,
+        y = 0;
+
+    function slider (selection) {
+
+        //Line to represent the current value
+        var valueLine = selection.append("line")
+            .attr("x1", x)
+            .attr("x2", x + (width * value))
+            .attr("y1", y)
+            .attr("y2", y)
+            .style({stroke: "#51CB3F",
+                    "stroke-linecap": "round",
+                    "stroke-width": 6 });
+
+        //Line to show the remaining value
+        var emptyLine = selection.append("line")
+            .attr("x1", x + (width * value))
+            .attr("x2", x + width)
+            .attr("y1", y)
+            .attr("y2", y)
+            .style({
+                "stroke": "#ECECEC",
+                "stroke-linecap": "round",
+                "stroke-width": 6
+            });
+
+        var drag = d3.behavior.drag().on("drag", function() {
+            var newX = d3.mouse(this)[0];
+
+            if (newX < x)
+                newX = x;
+            else if (newX > x + width)
+                newX = x + width;
+
+            value = (newX - x) / width;
+            valueCircle.attr("cx", newX);
+            valueLine.attr("x2", x + (width * value));
+            emptyLine.attr("x1", x + (width * value));
+
+            if (event)
+                event();
+
+            d3.event.sourceEvent.stopPropagation();
+        })
+
+        //Draggable circle to represent the current value
+        var valueCircle = selection.append("circle")
+            .attr("cx", x + (width * value))
+            .attr("cy", y)
+            .attr("r", 8)
+            .style({
+                "stroke": "black",
+                "stroke-width": 1.0,
+                "fill": "white"
+            })
+            .call(drag);
+    }
+
+
+    slider.x = function (val) {
+        x = val;
+        return slider;
+    }
+
+    slider.y = function (val) {
+        y = val;
+        return slider;
+    }
+
+    slider.value = function (val) {
+        if (val) {
+            value = val;
+            return slider;
+        } else {
+            return value;
+        }
+    }
+
+    slider.width = function (val) {
+        width = val;
+        return slider;
+    }
+
+    slider.event = function (val) {
+        event = val;
+        return slider;
+    }
+
+    return slider;
 }
 
 d3.select('#slider1')
@@ -161,3 +304,13 @@ d3.select('#slider1')
         time = value
         updatePlot()
   }));
+
+
+var slider1 = new simpleSlider();
+slider1.width(200).x(30).y(200).value(1.0).event(function(){
+  console.log(slider1.value());
+  d3.selectAll('.dot-Cell-1').style("opacity", slider1.value());
+});
+
+d3.select("#cells").append("svg").attr("width", 1000).attr("height", 700).call(slider1);
+      
